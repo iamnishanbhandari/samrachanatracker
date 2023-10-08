42f6f9ac5cc8aaf5744996730f6fab96de0d5d50
Nishan Bhandari <bnishan18@gmail.com>
2023-10-08 07:39:15 +0545

    SeedCertification done

---
+++ src/components/pages/SeedCetificationAuthority.js
@@ -0,0 +1,62 @@
+import React from "react";
+import NavBar from "../navbar/NavBar";
+
+const SeedCetificationAuthority = () => {
+  return (
+    <>
+      <NavBar />
+      <div class="flex justify-center mt-32 bg-#F7EEF3">
+        <div class="border border-gray-300 flex flex-col justify-center w-96 h-80 rounded-xl">
+          <div>
+            <input
+              type="text"
+              class="border border-gray-300 px-2 py-4 m-2 w-80 rounded-md focus:outline-none focus:ring focus:border-blue-500  "
+              placeholder="Enter Tag no "
+            ></input>
+          </div>
+
+          <div>
+            <input
+              type="number"
+              class="border border-gray-300 px-2 py-4  m-2 w-80 rounded-md focus:outline-none focus:ring focus:border-blue-500  "
+              placeholder="Enter Certification info"
+            ></input>
+          </div>
+
+          <div>
+            <input
+              type="date"
+              class="border border-gray-300 px-2 py-4 m-2 w-80 rounded-md focus:outline-none focus:ring focus:border-blue-500  "
+              placeholder="Enter Issuing Date"
+            ></input>
+          </div>
+
+          <div>
+            <button class="bg-gray-800 text-white m-2 py-2 px-2 rounded-xl w-80">
+              Update Details
+            </button>
+          </div>
+
+          <div>
+            <input
+              type="number"
+              class="border border-gray-300 px-2 py-4  m-2 w-80 rounded-md focus:outline-none focus:ring focus:border-blue-500  "
+              placeholder="Enter Validity"
+            ></input>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <button class=" outline-none h-14 rounded-md flex w-32 mx-20 my-24 p-1 bg-blue-900 text-white font-semi-bold ">
+            {" "}
+            Show Transcactions
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SeedCetificationAuthority;
